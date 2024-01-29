26348d7ee8f80bd476ca6d47c654b940fac77168
ladilaci <143039239+ladilaci@users.noreply.github.com>
2024-01-29 14:02:09 +0100

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM ev_data;
+ALTER TABLE ev_data
+DROP COLUMN category;
+SELECT *
+FROM ev_data;
+ALTER TABLE ev_data
+DROP COLUMN parameter;
+SELECT *
+FROM ev_data;
+ALTER TABLE ev_data
+DROP COLUMN unit;
+SELECT *
+FROM ev_data;
